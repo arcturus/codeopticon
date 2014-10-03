e3750f68fd0465b382191f4fd0c5cb43fac8e165
Francisco Jordano <fjordano@mozilla.com>
2014-10-03 17:59:21 +0100

    Doing form post via ajax, still missing the refresh

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,22 +2,50 @@
 
 (function(exports) {
 
+  var repo,
+      form;
+
   // Initialise all the thinigs \o/
   function start() {
-    var repo = document.getElementById('repos');
+    repo = document.getElementById('repos');
     // First screen can be null
     if (repo) {
       repo.addEventListener('click', clickHandler);
     }
+
+    form = document.getElementById('addSpy');
+    form.addEventListener('submit', addNewSpy);
   }
 
   // Delegate all the clicks here ... what could go wrong?
   function clickHandler(evt) {
-    
+
+  }
+
+  function addNewSpy(e) {
+    e.preventDefault();
+    var url = form.querySelector('[name="githubUrl"]').value.trim();
+    if (!url || url.length === 0) {
+      return;
+    }
+
+    $.post(form.action, {
+        githubUrl: url
+      },
+      refreshRepos
+    ).fail(function() {
+      alert('Could not add spy');
+    });
+    return false;
+  }
+
+  function refreshRepos() {
+    alert('Refrescando repos');
   }
 
   var App = {
     start: start,
+    refreshRepos: refreshRepos
   }
 
   exports.App = App;
